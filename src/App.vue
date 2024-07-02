4b61b6fcc9ea4561d58fe12c968df4474adbf82e
Joshua Langeveldt <langeveldt20joshua@gmail.com>
2024-07-02 14:40:25 +0200

    AboutPage Content Added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="wrapper">
+    <NavComp />
+    <main class="container-fluid">
+      <router-view />
+    </main>
+    <FooterComp />
+  </div>
+
 </template>
 
+<script>
+import NavComp from '@/components/Navbar.vue' 
+import FooterComp from '@/components/Footer.vue'
+
+export default {
+  components: {
+    NavComp,
+    FooterComp
+  },
+  computed: {
+    jobTitle(){
+      return this.$store.state.jobTitle
+    }
+  },
+
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
